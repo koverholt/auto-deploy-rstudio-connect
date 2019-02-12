acbfe76ae958f29e6868e7ad665dbcefc0c1b886
Kristopher Overholt <koverholt@gmail.com>
2019-02-11 22:35:08 -0600

    Fix formatting of upload and deploy script

--- upload_and_deploy.sh
+++ upload_and_deploy.sh
@@ -5,53 +5,54 @@
 #
 # Run this script from the content root directory.
 #
- set -e
- if [ -z "${CONNECT_SERVER}" ] ; then
-    echo "The CONNECT_SERVER environment variable is not defined. It defines"
-    echo "the base URL of your RStudio Connect instance."
-    echo
-    echo "    export CONNECT_SERVER='http://connect.company.com/'"
-    exit 1
+
+set -e
+set -x
+
+if [ -z "${CONNECT_SERVER}" ] ; then
+  echo "The CONNECT_SERVER environment variable is not defined. It defines"
+  echo "the base URL of your RStudio Connect instance."
+  echo
+  echo "    export CONNECT_SERVER='http://connect.company.com/'"
+  exit 1
 fi
- if [[ "${CONNECT_SERVER}" != */ ]] ; then
-    echo "The CONNECT_SERVER environment variable must end in a trailing slash. It"
-    echo "defines the base URL of your RStudio Connect instance."
-    echo
-    echo "    export CONNECT_SERVER='http://connect.company.com/'"
-    exit 1
+if [[ "${CONNECT_SERVER}" != */ ]] ; then
+  echo "The CONNECT_SERVER environment variable must end in a trailing slash. It"
+  echo "defines the base URL of your RStudio Connect instance."
+  echo
+  echo "    export CONNECT_SERVER='http://connect.company.com/'"
+  exit 1
 fi
- if [ -z "${CONNECT_API_KEY}" ] ; then
-    echo "The CONNECT_API_KEY environment variable is not defined. It must contain"
-    echo "an API key owned by a 'publisher' account in your RStudio Connect instance."
-    echo
-    echo "    export CONNECT_API_KEY='jIsDWwtuWWsRAwu0XoYpbyok2rlXfRWa'"
-    exit 1
+if [ -z "${CONNECT_API_KEY}" ] ; then
+  echo "The CONNECT_API_KEY environment variable is not defined. It must contain"
+  echo "an API key owned by a 'publisher' account in your RStudio Connect instance."
+  echo
+  echo "    export CONNECT_API_KEY='jIsDWwtuWWsRAwu0XoYpbyok2rlXfRWa'"
+  exit 1
 fi
-#  if [ $# -ne 1 ] ; then
-#     echo "usage: $0 <app-guid>"
-#     exit 1
-# fi
+
 APP="$1"
 BUNDLE_PATH="./bundle.tar.gz"
 CONTENT_DIRECTORY="$2"
+
 echo "APP GUID: ${APP}"
- # Remove any bundle from previous attempts.
+
+# Remove any bundle from previous attempts
 rm -f "${BUNDLE_PATH}"
- # Create an archive with all of our application source and data.
+
+ # Create an archive with all of our application source and data
 echo "Creating bundle archive: ${BUNDLE_PATH}"
-#tar czf "${BUNDLE_PATH}" manifest.json app.R data
 tar czf "${BUNDLE_PATH}" -C "${CONTENT_DIRECTORY}" .
 
- # Upload the bundle
-# TODO: Make this a v1 path
+# Upload the bundle
 UPLOAD=$(curl --silent --show-error -k -L --max-redirs 0 --fail -X POST \
               -H "Authorization: Key ${CONNECT_API_KEY}" \
               --data-binary @"${BUNDLE_PATH}" \
               "${CONNECT_SERVER}__api__/v1/experimental/content/${APP}/upload")
 BUNDLE=$(echo "$UPLOAD" | jq -r .bundle_id)
 echo "Created bundle: $BUNDLE"
- # Deploy the bundle.
-# TODO: Make this a v1 path
+
+# Deploy the bundle
 DEPLOY=$(curl --silent --show-error -k -L --max-redirs 0 --fail -X POST \
               -H "Authorization: Key ${CONNECT_API_KEY}" \
               --data '{"bundle":'"${BUNDLE}"'}' \
@@ -59,8 +60,8 @@ DEPLOY=$(curl --silent --show-error -k -L --max-redirs 0 --fail -X POST \
 
 echo "$DEPLOY"
 TASK=$(echo "$DEPLOY" | jq -r .id)
- # Poll until task has completed.
-# TODO: Make this a v1 path
+
+# Poll until task has completed
 FINISHED=false
 CODE=-1
 START=0
@@ -68,12 +69,12 @@ echo "Deployment task: ${TASK}"
 while [ "${FINISHED}" != "true" ] ; do
     DATA=$(curl --silent --show-error -k -L --max-redirs 0 --fail \
               -H "Authorization: Key ${CONNECT_API_KEY}" \
-              "${CONNECT_SERVER}__api__/tasks/${TASK}?wait_time=1&first_status=$START")
-    # Extract parts of the task status.
+              "${CONNECT_SERVER}__api__/v1/experimental/tasks/${TASK}?wait_time=1&first_status=$START")
+    # Extract parts of the task status
     FINISHED=$(echo "${DATA}" | jq .finished)
     CODE=$(echo "${DATA}" | jq .code)
     START=$(echo "${DATA}" | jq .last_status)
-    # Present the latest status lines.
+    # Present the latest status lines
     echo "${DATA}" | jq  -r '.status | .[]'
 done
  if [ "${CODE}" -ne 0 ]; then
